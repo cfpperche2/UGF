403dedde5a361d83adf429b2271fec8763b9268a
cfpperche <cfpperche@gmail.com>
2014-10-03 19:06:32 -0300

    chapter 3 complete

---
+++ Assets/Scripts/Core/BaseInputController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Base/Input Controller")]
+
+public class BaseInputController : MonoBehaviour
+{
+
+    // directional buttons
+    public bool Up;
+    public bool Down;
+    public bool Left;
+    public bool Right;
+
+    // fire / action buttons
+    public bool Fire1;
+    public bool Fire2;
+    public bool ScrollUp;
+    public bool ScrollDown;
+
+    // weapon slots
+    public bool Slot1;
+    public bool Slot2;
+    public bool Slot3;
+    public bool Slot4;
+    public bool Slot5;
+    public bool Slot6;
+    public bool Slot7;
+    public bool Slot8;
+    public bool Slot9;
+
+    public float vert;
+    public float horz;
+    public bool shouldRespawn;
+
+    public Vector3 TEMPVec3;
+    private Vector3 zeroVector = new Vector3(0, 0, 0);
+
+    public virtual void CheckInput()
+    {
+        // override with your own code to deal with input
+        horz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+    }
+
+    public virtual float GetHorizontal()
+    {
+        // returns our cached horizontal input axis value
+        return horz;
+    }
+
+    public virtual float GetVertical()
+    {
+        // returns our cached vertical input axis value
+        return vert;
+    }
+
+    public virtual bool GetFire1()
+    {
+        return Fire1;
+    }
+
+    public virtual bool GetFire2()
+    {
+        return Fire2;
+    }
+
+    public virtual bool GetScrollUp()
+    {
+        return ScrollUp;
+    }
+
+    public virtual bool GetScrollDown()
+    {
+        return ScrollDown;
+    }
+
+    public bool GetRespawn()
+    {
+        return shouldRespawn;
+    }
+
+    public virtual Vector3 GetMovementDirectionVector()
+    {
+        // temp vector for movement dir gets set to the value of an otherwise unused vector that always have the value of 0,0,0
+        TEMPVec3 = zeroVector;
+
+        TEMPVec3.x = horz;
+        TEMPVec3.y = vert;
+
+        // return the movement vector
+        return TEMPVec3;
+    }
+
+}
---
+++ Assets/Scripts/Core/BaseUserManager.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Base/User Manager")]
+
+public class BaseUserManager : MonoBehaviour
+{
+    // gameplay specific data
+    // we keep these private and provide methods to modify them instead, just to prevent any
+    // accidental corruption or invalid data coming in
+    private int score;
+    private int highScore;
+    private int level;
+    private int health;
+    private bool isFinished;
+
+    // this is the display name of the player
+    public string playerName = "Anon";
+
+	// this is the display name of the player clan's
+	public string clanName = "None";
+
+    public virtual void GetDefaultData()
+    {
+        playerName = "Player";
+		clanName = "None";
+        score = 0;
+        level = 1;
+        health = 3;
+        highScore = 0;
+        isFinished = false;
+    }
+
+    public string GetPlayerName()
+    {
+        return playerName;
+    }
+
+    public void SetPlayerName(string aName)
+    {
+        playerName = aName;
+    }
+
+	public string GetClanName()
+	{
+		return clanName;
+	}
+	
+	public void SetClanName(string aName)
+	{
+		playerName = aName;
+	}
+
+    public int GetLevel()
+    {
+        return level;
+    }
+
+    public void SetLevel(int num)
+    {
+        level = num;
+    }
+
+    public int GetHighScore()
+    {
+        return highScore;
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public virtual void AddScore(int anAmount)
+    {
+        score += anAmount;
+    }
+
+    public void LostScore(int num)
+    {
+        score -= num;
+    }
+
+    public void SetScore(int num)
+    {
+        score = num;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void AddHealth(int num)
+    {
+        health += num;
+    }
+
+    public void ReduceHealth(int num)
+    {
+        health -= num;
+    }
+
+    public void SetHealth(int num)
+    {
+        health = num;
+    }
+
+    public bool GetIsFinished()
+    {
+        return isFinished;
+    }
+
+    public void SetIsFinished(bool aVal)
+    {
+        isFinished = aVal;
+    }
+
+}
