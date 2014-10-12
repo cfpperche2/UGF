9da8cfb748fb7d01c5f37b36417b656a4a1518f6
cfpperche <cfpperche@gmail.com>
2014-10-11 21:09:23 -0300

    chapter 4.6 cameras

---
+++ Assets/Scripts/Common/Camera/BaseCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Base/Camera Controller")]
+
+public class BaseCameraController : MonoBehaviour {
+	
+	public Transform cameraTarget;
+	
+	public virtual void SetTarget(Transform aTarget)
+	{
+		cameraTarget=aTarget;	
+	}
+}
